05440f3455c1411eea20392b4a74350f87fe4f9f
AmmarRabie <ammaralsayed55@gmail.com>
2020-05-18 12:09:28 +0200

    Add main web app. add required packages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import "antd/dist/antd.css";
 import './App.css';
+import "waveform-playlist/styles/playlist.scss"
+import { Layout, Menu, Avatar } from 'antd';
+
+import Routes from "routes"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Layout.Header>
+        {/* <Avatar shape="square" size={64} src="logo192.png" /> */}
+        <img src="logo.png" className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">About</Menu.Item>
+          <Menu.Item key="3">Profile</Menu.Item>
+        </Menu>
+      </Layout.Header>
+      <Layout.Content style={{ padding: '0 50px' }}>
+        <Routes />
+      </Layout.Content>
+      <Layout.Footer style={{ textAlign: 'center' }}>Voxilla ©2020 Created by Voxilla team at CUFE</Layout.Footer>
+    </Layout>
   );
 }
+
+
 
 export default App;
